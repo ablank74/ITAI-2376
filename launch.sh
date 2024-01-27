228063832d7cf0717051ee28d4a3c96e4670a395
Adam Blank <adam@12thfloor.com>
2024-01-26 23:39:40 -0600

    added docker permissions

--- launch.sh
+++ launch.sh
@@ -1,2 +1,16 @@
+#!/bin/bash
+
+
+if [[ $(id -nG) =~ "docker" ]] ; then
+    echo "$USER in Docker Group"
+else
+  # Add the current user to the Docker group
+  echo "Adding $USER to the Docker group"
+  sudo usermod -aG docker $USER
+  echo "User added to the Docker group and restarting Docker"
+  sudo systemctl restart docker
+fi
+
+
 docker build . -t itai-2376
 docker run -it --rm --gpus=all -p 8888:8888 -p 5000:5000 -v C/home/adam/GitRepos/ITAI-2376/notebooks:/root/notebooks itai-2376
