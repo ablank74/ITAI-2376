c93d83b5327e5adb02754455d3e16135ff8e0169
Adam Blank <adam@12thfloor.com>
2024-03-19 20:59:16 -0500

    cleaned up pathing

---
+++ aws-Modules/aws_launch.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+
+if [[ $(id -nG) =~ "docker" ]] ; then
+    echo "$USER in Docker Group"
+else
+  # Add the current user to the Docker group
+  echo "Adding $USER to the Docker group"
+  sudo usermod -aG docker $USER
+  echo "User added to the Docker group and restarting Docker"
+  sudo systemctl restart docker
+fi
+
+#prompt if we should rebuild the container
+read -p "Do you want to rebuild the container? (y/n) " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    echo "Rebuilding the container"
+    docker build . -t aws-itai-2376
+fi
+docker run -it --rm --gpus=all -p 8888:8888 -p 5000:5000 -v /home/adam/GitRepos/ITAI-2376/aws-notebooks:/root/notebooks aws-itai-2376
